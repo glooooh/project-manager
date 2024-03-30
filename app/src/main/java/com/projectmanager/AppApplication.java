bca10945685c55916d65f3e75e632829c13fab25
Marcos Vinicius Noberto de Oliveira <112330160+marcosnoberto@users.noreply.github.com>
2024-03-30 14:45:05 -0300

    Deletando o dialect

--- app/src/main/java/com/projectmanager/AppApplication.java
+++ app/src/main/java/com/projectmanager/AppApplication.java
@@ -1,14 +1,34 @@
 package com.projectmanager;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import githubService.SpringBootApplication;
 //import org.springframework.security.oauth2.core.OAuth2AccessToken;
 
-@SpringBootApplication()
-public class AppApplication {
+import com.projectmanager.entities.Projeto;
+import com.projectmanager.service.ProjetoService;
 
+@SpringBootApplication()
+public class AppApplication implements CommandLineRunner{
+
+	@Autowired
+	ProjetoService projetoService;
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+	}
+
+	//Teste para verificar integração com o BD
+	@Override
+	public void run(String... args) throws Exception {
+		Demo1();
+	}
+
+	private void Demo1(){
+		for(Projeto projeto : projetoService.findAll()){
+			System.out.println("id: "+ projeto.getId());
+			System.out.println("nome: "+projeto.getNome());
+		}
 	}
 }
