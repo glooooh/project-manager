d70750571bf2cdfe2f00056f7909263b5f086f13
glooooh <glorinhamsouza@gmail.com>
2024-05-15 21:09:00 -0300

    Prevenindo erros

--- app/src/main/java/com/projectmanager/service/ColaboradorService.java
+++ app/src/main/java/com/projectmanager/service/ColaboradorService.java
@@ -2,10 +2,12 @@ package com.projectmanager.service;
 
 import com.projectmanager.entities.Colaborador;
 import com.projectmanager.entities.Tarefa;
+import com.projectmanager.entities.Usuario;
 
 public interface ColaboradorService {
     public Iterable<Colaborador> findAll();
     public Colaborador save(Colaborador colaborador);
-    public Iterable<Tarefa> findTasksByID(int id);
+    public Iterable<Tarefa> findTasksByIDUser(int id);
+    public Iterable<Usuario> findCollaboratorsByIDTask(int id);
     public void deleteColaboradoresTarefa(int id);
 }
--- app/src/main/java/com/projectmanager/service/ColaboradorServiceImpl.java
+++ app/src/main/java/com/projectmanager/service/ColaboradorServiceImpl.java
@@ -7,14 +7,16 @@ import org.springframework.stereotype.Service;
 
 import com.projectmanager.entities.Colaborador;
 import com.projectmanager.entities.Tarefa;
+import com.projectmanager.entities.Usuario;
 import com.projectmanager.repositories.ColaboradorRepository;
 
 @Service("colaboradorService")
-public class ColaboradorServiceImpl implements ColaboradorService{
+public class ColaboradorServiceImpl implements ColaboradorService {
 
     @Autowired
     ColaboradorRepository colaboradorRepository;
     TarefaService tarefaService;
+    UsuarioService usuarioService;
 
     @Override
     public Iterable<Colaborador> findAll() {
@@ -23,27 +25,40 @@ public class ColaboradorServiceImpl implements ColaboradorService{
 
     @Override
     public Colaborador save(Colaborador colaborador) {
-       return colaboradorRepository.save(colaborador);
+        return colaboradorRepository.save(colaborador);
     }
 
     @Override
-    public Iterable<Tarefa> findTasksByID(int id) {
+    public Iterable<Tarefa> findTasksByIDUser(int id) {
         ArrayList<Tarefa> tarefas = new ArrayList<>();
-        
-        for(Colaborador colaborador : findAll()){
-			if(colaborador.getUsuario_id() == id){
+
+        for (Colaborador colaborador : findAll()) {
+            if (colaborador.getUsuario_id() == id) {
                 tarefas.add(tarefaService.find(colaborador.getTarefa_id()));
             }
-		}
+        }
 
         return tarefas;
 
     }
 
     @Override
+    public Iterable<Usuario> findCollaboratorsByIDTask(int id) {
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+
+        for (Colaborador colaborador : findAll()) {
+            if (colaborador.getTarefa_id() == id) {
+                usuarios.add(usuarioService.find(colaborador.getUsuario_id()));
+            }
+        }
+
+        return usuarios;
+    }
+
+    @Override
     public void deleteColaboradoresTarefa(int idTarefa) {
-       for (Colaborador colaborador : findAll()) {   
-            if(colaborador.getTarefa_id() == idTarefa){
+        for (Colaborador colaborador : findAll()) {
+            if (colaborador.getTarefa_id() == idTarefa) {
                 colaboradorRepository.delete(colaborador);
             }
         }
