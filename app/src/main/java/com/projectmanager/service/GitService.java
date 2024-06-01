dc3c6d945d85bef9bef4b58bb872698e5ef85941
davialmeida135 <davi.almeida.135@ufrn.edu.br>
2024-05-31 23:25:36 -0300

    inicio implementação da interface

--- app/src/main/java/com/projectmanager/service/GitService.java
+++ app/src/main/java/com/projectmanager/service/GitService.java
@@ -7,18 +7,19 @@ import org.kohsuke.github.GHUser;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 
+import com.projectmanager.exceptions.BusinessException;
 import com.projectmanager.model.RepositoryModel;
 import com.projectmanager.model.UsuarioModel;
 
 public interface GitService {
-    UsuarioModel getUser(String accessToken);
-    RepositoryModel getRepository(String owner, String repoName);
-    List<RepositoryModel> getRepositories(String owner);
-    Long getCollaboratorId(String collaboratorName, RepositoryModel repo);
+    UsuarioModel getUser(String accessToken)throws IOException;
+    RepositoryModel getRepository(String owner, String repoName)throws IOException;
+    List<RepositoryModel> getRepositories(String owner)throws IOException;
+    Long getCollaboratorId(String accessToken, String collaboratorName, RepositoryModel repo) throws IOException;
 
     void saveIssuesAsTarefas(RepositoryModel repo,TarefaService tarefaService);
     String getAccessToken(OAuth2AuthenticationToken authenticationToken, String clientRegistrationId,OAuth2AuthorizedClientService oauth2AuthorizedClientService);
-    String getUserId(OAuth2AuthenticationToken authenticationToken, OAuth2AuthorizedClientService oauth2AuthorizedClientService) ;
+    String getUserId(OAuth2AuthenticationToken authenticationToken, OAuth2AuthorizedClientService oauth2AuthorizedClientService)throws IOException ;
     boolean isAuthenticated(OAuth2AuthenticationToken authenticationToken);
-    boolean validateUser(UsuarioModel loggedUser, String userId);
+    boolean validateUser(UsuarioModel loggedUser, String userId)throws BusinessException;
 }
---
+++ app/src/main/java/com/projectmanager/service/GithubService2.java
@@ -0,0 +1,153 @@
+package com.projectmanager.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.kohsuke.github.GHMyself;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GHUser;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+import org.springframework.dao.PermissionDeniedDataAccessException;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+
+import com.projectmanager.exceptions.BusinessException;
+import com.projectmanager.model.RepositoryModel;
+import com.projectmanager.model.UsuarioModel;
+
+public class GithubService2 implements GitService{
+
+    GHMyself getGhMyself(String accessToken) throws IOException {
+        GitHub github = new GitHubBuilder().withOAuthToken(accessToken).build();
+        return github.getMyself();
+    }
+
+    @Override
+    public UsuarioModel getUser(String accessToken) throws IOException{
+        try {
+            GHMyself user = getGhMyself(accessToken);
+            UsuarioModel newUser = new UsuarioModel(user.getLogin(), user.getId(), accessToken,user.getEmail(),user.getName());
+            return newUser;
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            return null;
+        }
+        
+    }
+    
+
+    @Override
+    public RepositoryModel getRepository(String accessToken, String repoName) throws IOException {
+        GHMyself user = getGhMyself(accessToken);
+        List<RepositoryModel> repositories = getRepositories(accessToken);
+        for(RepositoryModel repo : repositories){
+            if(repo.getName().equals(repoName)){
+                return repo;
+            }
+        }
+        throw new IllegalArgumentException("Repositório não encontrado");
+        
+    }
+
+    @Override
+    public List<RepositoryModel> getRepositories(String accessToken) throws IOException{
+        GHMyself user = getGhMyself(accessToken);
+        Collection<GHRepository> repositories = user.getRepositories().values();
+        List<RepositoryModel> repositoryModels = new ArrayList<RepositoryModel>();
+        for(GHRepository oldRepo : repositories){
+            RepositoryModel newRepo = new RepositoryModel();
+            newRepo.setName(oldRepo.getName());
+            newRepo.setId(oldRepo.getId());
+            newRepo.setDescription(oldRepo.getDescription());
+            newRepo.setLanguage(oldRepo.getLanguage());
+            newRepo.setOwner(oldRepo.getOwnerName());
+            newRepo.setUrl(oldRepo.getUrl().toString());
+            newRepo.setBranches(oldRepo.getBranches().keySet());
+            newRepo.setCollaborators(oldRepo.getCollaboratorNames());
+            repositoryModels.add(newRepo);
+        }
+        return repositoryModels;
+    }
+
+    @Override
+    public Long getCollaboratorId(String accessToken, String collaboratorName, RepositoryModel repo) throws IOException{
+        GHMyself user = getGhMyself(accessToken);
+        
+        Collection<GHRepository> repositories = user.getRepositories().values();
+        GHRepository foundRepo = null;
+        for(GHRepository oldRepo : repositories){
+            if(oldRepo.getName().equals(repo.getName())){
+                foundRepo = oldRepo;
+                break;
+            }
+        }
+        if(foundRepo == null){
+            throw new IllegalArgumentException("Repositório não encontrado");
+        }
+        for(GHUser collaborator : foundRepo.getCollaborators()){
+            if(collaborator.getLogin().equals(collaboratorName)){
+                return collaborator.getId();
+            }
+        }
+        throw new IllegalArgumentException("Colaborador não encontrado");
+
+    }
+
+    @Override
+    public void saveIssuesAsTarefas(RepositoryModel repo, TarefaService tarefaService) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'saveIssuesAsTarefas'");
+    }
+
+    @Override
+    public String getAccessToken(OAuth2AuthenticationToken authenticationToken, String clientRegistrationId,
+            OAuth2AuthorizedClientService oauth2AuthorizedClientService) {
+        OAuth2AuthorizedClient client = oauth2AuthorizedClientService.loadAuthorizedClient(clientRegistrationId, authenticationToken.getName());
+
+        if (client != null) {
+            OAuth2AccessToken accessToken = client.getAccessToken();
+            if (accessToken != null) {
+                return accessToken.getTokenValue();
+            } else {
+                System.out.println("Access token is missing for client: " + clientRegistrationId);
+                throw new RuntimeException("Access token is missing for client"); 
+            } 
+        }
+        else {
+            System.out.println("Client not found for clientRegistrationId: " + clientRegistrationId);
+            throw new RuntimeException("Client missing"); 
+        }
+    }
+
+    //TODO Não sei se é necessario
+    @Override
+    public String getUserId(OAuth2AuthenticationToken authenticationToken,
+            OAuth2AuthorizedClientService oauth2AuthorizedClientService) throws IOException{
+                 if (isAuthenticated(authenticationToken)) {
+                    String accessToken = getAccessToken(authenticationToken, "github",
+                            oauth2AuthorizedClientService);
+                    UsuarioModel loggedUser = getUser(accessToken);
+                    return String.valueOf(loggedUser.getId());
+            }
+            return null; // Retornar null ou algum valor padrão se o usuário não estiver autenticado
+    }
+
+    @Override
+    public boolean isAuthenticated(OAuth2AuthenticationToken authenticationToken) {
+        return authenticationToken != null && authenticationToken.isAuthenticated();
+    }
+
+    @Override
+    public boolean validateUser(UsuarioModel loggedUser, String userId) throws BusinessException {
+        if (!userId.equals(Long.toString(loggedUser.getId()))) {
+            throw new BusinessException("Tentativa de acesso de repositório pertencente a outro usuário");
+        }
+        return true;
+    }
+    
+}
